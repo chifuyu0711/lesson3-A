668c90a6af9f61dcc12a02962e196b7482584a21
Muxotxon <saidalixonov@gmail.com>
2023-03-10 17:07:41 +0300

    end

---
+++ main.js
@@ -0,0 +1,17 @@
+//JavaScript 3 dz  A
+
+
+let number = +prompt('Введите число')
+let stepen = +prompt('Введите степень')
+let otvet = 1
+
+
+
+for (let i = 1; i <= stepen ; i++) {
+
+    otvet = number * otvet
+    
+}
+
+
+alert(otvet);
